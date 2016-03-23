d1695d3b4238b3e69c67ed21ff6edd7b6e0fdcf7
kleuter <support@crystalidea.com>
2016-03-23 17:39:50 +0100

    fix for QMacStyle::drawControl

--- 5.3.2/qtbase/src/widgets/styles/qmacstyle_mac.mm
+++ 5.3.2/qtbase/src/widgets/styles/qmacstyle_mac.mm
@@ -3667,9 +3667,9 @@ void QMacStyle::drawControl(ControlElement ce, const QStyleOption *opt, QPainter
 
                 NSBezierPath *pushButtonFocusRingPath;
                 if (bdi.kind == kThemeBevelButton)
-                    pushButtonFocusRingPath = [NSBezierPath bezierPathWithRect:focusRect];
+                    pushButtonFocusRingPath = [NSBezierPath bezierPathWithRect:NSRectFromCGRect(focusRect)];
                 else
-                    pushButtonFocusRingPath = [NSBezierPath bezierPathWithRoundedRect:focusRect xRadius:4 yRadius:4];
+                    pushButtonFocusRingPath = [NSBezierPath bezierPathWithRoundedRect:NSRectFromCGRect(focusRect) xRadius:4 yRadius:4];
                 qt_drawFocusRingOnPath(cg, pushButtonFocusRingPath);
             }
 
