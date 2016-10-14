cafe01d888e0efc53f15a5b85661b02e82b5c6cd
kleuter <support@crystalidea.com>
2016-10-14 18:09:35 +0200

    qt compile x64 5.6.2

---
+++ 5.6.2/qt5vars_x64.cmd
@@ -0,0 +1,3 @@
+%comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"" amd64
+set PATH=c:\qt_x64\qtbase\bin;c:\qt_x64\gnuwin32\bin;%PATH%
+set QMAKESPEC=win32-msvc2013
