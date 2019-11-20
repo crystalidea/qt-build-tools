e8752bdb64ba0093033c019efb3c7e47b66047bb
kleuter <support@crystalidea.com>
2019-11-20 12:12:41 +0100

    5.13.2

---
+++ 5.13.2/compile_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export PATH=$PATH:/usr/local/Qt-5.13.2/bin
+
+cd qtbase
+
+./configure -opensource -confirm-license -nomake examples -nomake tests -no-openssl -securetransport
+
+make -j 12
+echo maki | sudo -S sudo make install
+
+cd ../qttools
+qmake
+make -j 12
+echo maki | sudo -S sudo make install
+
+cd ../qtmacextras
+qmake
+make -j 12
+echo maki | sudo -S sudo make install
+
+cd /usr/local
+zip -r ~/Desktop/qt5.13.2_mac.zip Qt-5.13.2/*
