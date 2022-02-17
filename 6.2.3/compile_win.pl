c76635d4ccfe97c919d2aeeee155f7bbef56209f
kleuter <support@crystalidea.com>
2022-02-17 15:14:17 +0100

    Revert "6.2.3 qtcompat module without qdeclarative dependencies"
    This reverts commit 6a832ed6cf0f92e6729e0354155e6bb3ce068d37.

--- 6.2.3/compile_win.pl
+++ 6.2.3/compile_win.pl
@@ -65,9 +65,7 @@ printLineToBat (":OPENSSL_ALREAD_COMPILED");
 
 # openssl: see https://bugreports.qt.io/browse/QTBUG-65501
 
-printLineToBat ("7z x qt5compat_no_qml.7z");
-
-my $skipped_modules = "qt3d qtactiveqt qtcharts qtcoap qtconnectivity qtdatavis3d qtdeclarative qtdoc qtlottie qtmqtt qtmultimedia qtnetworkauth qtopcua qtpositioning qtquick3d qtquicktimeline qtremoteobjects qtscxml qtsensors qtserialbus qtserialport qtshadertools qtsvg qttranslations qtvirtualkeyboard qtwayland qtwebchannel qtwebengine qtwebsockets qtwebview";
+my $skipped_modules = "qt3d qt5compat qtactiveqt qtcharts qtcoap qtconnectivity qtdatavis3d qtdeclarative qtdoc qtlottie qtmqtt qtmultimedia qtnetworkauth qtopcua qtpositioning qtquick3d qtquicktimeline qtremoteobjects qtscxml qtsensors qtserialbus qtserialport qtshadertools qtsvg qttranslations qtvirtualkeyboard qtwayland qtwebchannel qtwebengine qtwebsockets qtwebview";
 
 my $skipped_modules_cmd;
 
