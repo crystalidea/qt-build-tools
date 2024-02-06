55837e7bd5ff0671a814cb59e98ec722db827522
kleuter <support@crystalidea.com>
2024-02-06 12:08:25 +0100

    5.5.16, with openssl 3.0.13, code fixes stay the same, just added Sonoma enum

---
+++ 5.15.16/compile_mac.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then echo "Please specify the admin pass as first argument"; exit 1; fi
+
+makej () { 
+   make -j$(sysctl -n hw.ncpu) 
+}
+export PATH=$PATH:$(pwd)/qtbase/bin
+
+cd qtbase
+
+./configure QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" -opensource -confirm-license -nomake examples -nomake tests -no-openssl -securetransport
+
+makej
+echo $1 | sudo -S sudo make install
+
+cd ../qttools
+qmake
+makej
+echo $1 | sudo -S sudo make install
+
+cd ../qtmacextras
+qmake
+makej
+echo $1 | sudo -S sudo make install
+
+cd /usr/local
+zip -r ~/Desktop/qt5.15.16_mac.zip Qt-5.15.16/*
